4becf9c95f5b23d0f9b263304724cde84536b5d8
Rishikesh <148840508+RISHIKEsH12321@users.noreply.github.com>
2024-07-07 18:53:46 +0800

    CSS Update

---
+++ public/JavaScript/industry.js
@@ -0,0 +1,29 @@
+const obserever = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting){
+            entry.target.classList.replace("hidden", "show");
+        }else{
+            entry.target.classList.replace("show", "hidden");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => obserever.observe(el));
+
+document.addEventListener("DOMContentLoaded", () => {
+    const challengeContainers = document.querySelectorAll("#ChallengeIntro > div");
+
+    challengeContainers.forEach((container, index) => {
+        container.addEventListener("click", () => {
+            scrollToChallenge(index + 1); // Index starts from 0, so add 1 for challenge numbers starting from 1
+        });
+    });
+
+    function scrollToChallenge(challengeNumber) {
+        const challengeSection = document.querySelector(`#ChallengeContainer > div:nth-of-type(${challengeNumber})`);
+        if (challengeSection) {
+            challengeSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    }
+});
